2b2d8bd670ebf39810ebe5c70e6cadc46a1dfc58
Linh Tran <linhtanl51@gmail.com>
2021-04-15 18:14:32 -0500

    add header and change header height

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import RichTextEditor from './RichTextEditor';
 function App() {
   return (
     <div className="App">
+      <div className='App-header'>Your awesome text editor!</div>
       <RichTextEditor/>
     </div>
   );
